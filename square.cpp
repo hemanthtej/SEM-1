0197351c3e370e9665f5d06e85ed7ef90cc9057e
j.teja <j.teja1920@gmail.com>
2025-12-04 14:34:50 +0530

    Update square.cpp

--- square.cpp
+++ square.cpp
@@ -3,10 +3,10 @@
 int main()
 {
 double num, root;
-// Input
 printf("Enter an integer: ");
 scanf("%lf", &num);
 root = sqrt(num);
 printf("The Square Root of %.2lf is %.2lf.", num, root);
 return 0;
 }
+
