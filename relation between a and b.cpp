6cf1a5a405cb91f77d3fca94bc9b9ecd370fc055
j.teja <j.teja1920@gmail.com>
2025-12-04 14:33:53 +0530

    Update relation between a and b.cpp

--- relation between a and b.cpp
+++ relation between a and b.cpp
@@ -7,9 +7,9 @@ int main()
 	scanf("%d",&a);
 	printf("enter the value of b :");
 	scanf("%d",&b);
-	//performing operations on a,b
 	printf("a is less than b %d \n",a<b);
 	printf("a is greater than b %d \n",a>b);
 	printf("a is less than or equal to b %d \n",a<=b);
 	printf("a is greater than or equal to b %d \n",a>=b);
 }
+
